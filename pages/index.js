795bf9d7e1b7b202196075fddc8bb06ec11bc2a4
Ricardo Pereira <ricardodatadev@gmail.com>
2025-04-15 22:07:52 -0300

    adicona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Olá, seja muito bem vindo!</h1>
+  return <h1>Olá, seja muito bem vindo!</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
