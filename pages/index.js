6adf8ca21f16b41e3c75d7c1f0d05ac2f7d074d2
Ricardo Pereira <ricardodatadev@gmail.com>
2025-09-21 22:48:09 -0300

    Atualizando conteúdo

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,5 @@
 function Home() {
-  return <h1>Olá, seja muito bem vindo!</h1>;
+  return <h1>Olá, se você me ama, dá uma risadinha!</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identação errada");
-}
+export default Home;
